45707b8d005aaaa163718ec460710c444dddc0f5
Dmitriy Kondratovich <dvkgroup@gmail.com>
2023-12-02 08:22:32 +0300

    add 1160

--- 1160/main.go
+++ 1160/main.go
@@ -1,5 +1,40 @@
 package main
 
 func countCharacters(words []string, chars string) int {
-	return 0
+	mChars := getWordMap(chars)
+
+	var res int
+	var f bool
+	for i := 0; i < len(words); i++ {
+		mWord := getWordMap(words[i])
+
+		for k, v := range mWord {
+			if v2, ok := mChars[k]; ok {
+				if v > v2 {
+					f = false
+					break
+				}
+				f = true
+			} else {
+				f = false
+				break
+			}
+		}
+
+		if f {
+			res += len(words[i])
+			f = false
+		}
+	}
+
+	return res
+}
+
+func getWordMap(word string) map[rune]int {
+	m := make(map[rune]int, len(word))
+	for _, v := range word {
+		m[v]++
+	}
+
+	return m
 }
