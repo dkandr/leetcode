479fdc4ae3ce3fc85e57dd2cc2e81d24a800dfca
Dmitriy Kondratovich <dvkgroup@gmail.com>
2023-12-06 13:56:25 +0300

    update 1716

--- 1716/main.go
+++ 1716/main.go
@@ -1,18 +1,34 @@
 package main
 
+// func totalMoney(n int) int {
+// 	var res int
+//
+// 	m := make(map[int]int, 8)
+// 	m[0] = 0
+// 	for i := 1; i <= 7; i++ {
+// 		m[i] = m[i-1] + i
+// 	}
+//
+// 	w := n / 7
+// 	d := n % 7
+//
+// 	res = m[7]*w + 7*(w*(w-1)/2) + m[d] + d*w
+//
+// 	return res
+// }
+
 func totalMoney(n int) int {
 	var res int
 
-	m := make(map[int]int, 8)
-	m[0] = 0
+	sl := make([]int, 8)
 	for i := 1; i <= 7; i++ {
-		m[i] = m[i-1] + i
+		sl[i] = sl[i-1] + i
 	}
 
 	w := n / 7
 	d := n % 7
 
-	res = m[7]*w + 7*(w*(w-1)/2) + m[d] + d*w
+	res = sl[7]*w + 7*(w*(w-1)/2) + sl[d] + d*w
 
 	return res
 }
